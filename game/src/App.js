921828fb1d268c3f2447d07bd54b890014a877d9
Цифровая Трансфомация <123804884+CifraTrans@users.noreply.github.com>
2023-02-28 20:55:51 +0300

    test

---
+++ game/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  Welcome from './modules/game'
+
+function App() {
+  return (
+    <div className="App">
+    <Welcome name ="jo" / >
+          </div>
+  );
+}
+
+export default App;
---
+++ game/src/modules/game.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Welcome =(props)=>{
+    return(
+    `your name is ${props.name}`
+    )
+}
+
+export default Welcome;
